00657becc30a1ea2873b958147a243dbf9bacd59
Prashant Sinha <prashant+git@noop.pw>
2016-09-09 22:20:22 +0530

    Combined colorize and huestr modules

--- hues/huestr.py
+++ hues/huestr.py
@@ -1,29 +1,46 @@
 import sys
+from functools import partial
 
-from .colortable import KEYWORDS, SEQ
+from .colortable import FG, BG, HI_FG, HI_BG, SEQ, STYLE, KEYWORDS
+from .dpda import zero_break, annihilator, dedup, apply
+
+
+OPTIMIZATION_STEPS = (
+  zero_break,               # Handle Resets using `reset`.
+  annihilator(FG + HI_FG),  # Squash foreground colors to the last value.
+  annihilator(BG + HI_BG),  # Squash background colors to the last value.
+  dedup,                    # Remove duplicates in (remaining) style values.
+)
+optimize = partial(apply, OPTIMIZATION_STEPS)
+
+def colorize(string, stack):
+  '''Apply optimal ANSI escape sequences to the string.'''
+  codes = optimize(stack)
+  if len(codes):
+    prefix = SEQ % ';'.join(map(str, codes))
+    suffix = SEQ % STYLE.reset
+    return prefix + string + suffix
+  else:
+    return string
 
 
 class BaseHues(str):
   '''Extend the string class to support hues.'''
-
   def __new__(cls, string, hue_stack=None):
     '''Return a new instance of the class.'''
     return super(BaseHues, cls).__new__(cls, string)
 
   def __init__(self, string, hue_stack=tuple()):
-    self.string = string
-    self._hue_stack = hue_stack
+    self.__string = string
+    self.__hue_stack = hue_stack
 
   def __getattr__(self, attr):
     try:
       code = getattr(KEYWORDS, attr)
-      hues = self._hue_stack + (code,)
-      return BaseHues(self.string, hue_stack=hues)
+      hues = self.__hue_stack + (code,)
+      return BaseHues(self.__string, hue_stack=hues)
     except AttributeError as e:
       raise e
 
-  def _apply_hues_(self):
-    pass
-
   def __str__(self):
-    pass
+    return colorize(self.__string, self.__hue_stack)
