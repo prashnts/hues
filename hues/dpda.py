96317a488e0c5fd3e7b97d9ef34b78aecad99a08
Prashant Sinha <prashant+git@noop.pw>
2016-09-09 18:47:21 +0530

    Implemented helper methods for DPDA

--- hues/dpda.py
+++ hues/dpda.py
@@ -1,6 +1,32 @@
-'''Deterministic Push Down Automaton.
+'''Deterministic Push Down Automaton helpers.
 
-This module implements a DPDA to allow for producing deterministic
+This module implements helper functions to allow producing deterministic
 representation of arbitrarily chained props.
 '''
+from functools import reduce
 
+
+def zero_break(stack):
+  '''Handle Resets in input stack.
+  Breaks the input stack if a Reset operator (zero) is encountered.
+  '''
+  reducer = lambda x, y: tuple() if y == 0 else x + (y,)
+  return reduce(reducer, stack, tuple())
+
+def annihilate(stack, predicate):
+  '''Squash and reduce the input stack.
+  Removes the elements of input that match predicate and only keeps the last
+  match at the beginning of stack.
+  '''
+  # Take the last matching element.
+  n = next(filter(lambda x: x in predicate, stack[::-1]))
+  # Take remaining elements.
+  E = tuple(filter(lambda x: x not in predicate, stack))
+  return (n,) + E
+
+def dedup(stack):
+  '''Remove duplicates from the stack in first-seen order.'''
+  # Initializes with an accumulator and then reduces the stack with first match
+  # deduplication.
+  reducer = lambda x, y: x if y in x else x + (y,)
+  return reduce(reducer, stack, tuple())
--- tests/test_dpda.py
+++ tests/test_dpda.py
@@ -1,8 +1,17 @@
-import hues.dpda as dpda
+import hues.dpda as DPDA
 
+reduce = lambda x: x
 
 def test_zero_negation():
-  # Identity function for future tests.
-  reduce = lambda x: x
-  assert reduce((1, 2, 3, 4, 0, 10, 1)) == (10, 1)
-  assert reduce((1, 2, 3, 4, 5, 0)) == (0,)
+  func = DPDA.zero_break
+  assert func((1, 2, 3, 4, 0, 10, 1)) == (10, 1)
+  assert func((1, 2, 3, 4, 5, 0)) == tuple()
+
+def test_order_annihilation():
+  func = DPDA.annihilate
+  assert func((1, 2, 3, 4, 4, 3), range(0, 10)) == (3,)
+  assert func((1, 2, 10, 11, 11, 2), range(5, 12)) == (11, 1, 2, 2)
+
+def test_dedup():
+  func = DPDA.dedup
+  assert func((1, 2, 3, 3, 4, 2, 1, 3, 5)) == (1, 2, 3, 4, 5)
