68103d85ae430879a034da70076916fd2c4055a7
Prashant Sinha <prashant+git@noop.pw>
2016-10-02 22:02:43 +0530

    Syntactic sugar for better api usage.

--- hues/__init__.py
+++ hues/__init__.py
@@ -1,4 +1,22 @@
 # Unicorns
 from .huestr import HueString as hue
+from .console import Config, SimpleConsole, PowerlineConsole
 
-__version__ = (0, 1, 1)
+__version__ = (0, 2, 0)
+
+conf = Config()
+
+if conf.opts.theme == 'simple':
+  console = SimpleConsole(conf=conf)
+elif conf.opts.theme == 'powerline':
+  console = PowerlineConsole(conf=conf)
+
+log = console.log
+info = console.info
+warn = console.warn
+error = console.error
+success = console.success
+
+del conf
+
+__all__ = ('hue', 'console', 'log', 'info', 'warn', 'error', 'success')
