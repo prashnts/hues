4245ccf3a964d76f500622ea134295f49f88afc9
Prashant Sinha <prashant+git@noop.pw>
2016-10-02 15:01:36 +0530

    Saner config and SimpleConsole implementation

--- hues/console.py
+++ hues/console.py
@@ -3,8 +3,10 @@
 import os
 import sys
 import yaml
+from datetime import datetime
 
 from .huestr import HueString
+from .colortable import FG, BG, HI_FG, HI_BG, SEQ, STYLE, KEYWORDS
 
 if sys.version_info.major == 2:
   str = unicode # noqa
@@ -56,11 +58,33 @@ class _Console(object):
     conf.update(local_conf)
     return conf
 
-  def _base_log(self, *args, newline=True):
+  def _base_log(self, *args, **kwargs):
     for arg in args:
       if isinstance(arg, HueString):
         self.stdout.write(arg.colorized)
       else:
         self.stdout.write(str(arg))
-    if newline:
+    if kwargs.get('newline', True):
       self.stdout.write('\n')
+
+
+class SimpleConsole(_Console):
+  def log(self, *args):
+    '''Generate a simple log string.
+    Format: [{time}] - {messages}
+    '''
+    payload = []
+    if self.config['showtime']:
+      time = '[%s] - ' % datetime.now().strftime(self.config['timefmt'])
+      hue = getattr(FG, self.config['colors']['time'])
+      payload.append(HueString(time, hue_stack=(hue,)))
+    self._base_log(*payload, *args)
+
+  def error(self, *args):
+    pass
+
+  def warn(self, *args):
+    pass
+
+  def info(self, *args):
+    pass
